70e1a31c68c56e917fd8e839ac00d7387a8d2f5c
neilmd <neil.capistrano2007@gmail.com>
2024-11-30 07:57:33 -0700

    Calendar: Added forms

--- script/schedule.js
+++ script/schedule.js
@@ -1,6 +1,33 @@
 // Calendar
 var calendarEl = document.getElementById('calendar');
 var calendar = new FullCalendar.Calendar(calendarEl, {
-    initialView: 'dayGridMonth'
+    initialView: 'dayGridMonth',
+    eventSources: [
+
+        // your event source
+        {
+          events: [ // put the array in the `events` property
+            {
+              title  : 'event1',
+              start  : '2024-11-30'
+            },
+            {
+              title  : 'event2',
+              start  : '2024-11-10',
+              end    : '2024-11-17'
+            },
+            {
+              title  : 'event3',
+              start  : '2024-12-01',
+            }
+          ],
+          color: '#2c3e50',     // an option!
+          textColor: 'white' // an option!
+        }
+    
+        // any other event sources...
+    
+    ]
 });
 calendar.render();
+
