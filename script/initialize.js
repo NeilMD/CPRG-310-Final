a55a1a6473e0c0ba5aa35efc2747657f4bc5870c
neilmd <neil.capistrano2007@gmail.com>
2024-12-09 03:10:16 -0700

    Style Adjustment in homepage

--- script/initialize.js
+++ script/initialize.js
@@ -11,3 +11,21 @@ function loadTemplate(templatePath, containerId) {
 // Load the header and footer templates
 loadTemplate('./templates/header.html', 'header-container');
 loadTemplate('./templates/footer.html', 'footer-container');
+
+
+let lastScrollTop = 0; // Store the last scroll position
+
+window.onscroll = function() {
+  const header = document.getElementById("header-nav");
+  const currentScroll = document.documentElement.scrollTop;
+  
+  if (header != null){
+    if (currentScroll > lastScrollTop && currentScroll > 100) {
+        header.classList.add("hidden-header");
+    } else {
+        header.classList.remove("hidden-header");
+    }
+  }
+
+  lastScrollTop = currentScroll;
+};
